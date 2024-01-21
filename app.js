dc38a2c9f370154467bf939d8aed67dd63741be3
george-avinash <georgeavinash6@gmail.com>
2024-01-21 22:57:12 +0530

    done

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import userRouter from "./routes/user-routes.js";
+import adminRouter from "./routes/admin-routes.js";
+import movieRouter from "./routes/movie-routes.js";
+import bookingsRouter from "./routes/booking-routes.js";
+dotenv.config();
+const app = express();
+// const cors = require('cors');
+
+// const PORT = process.env.PORT || 5000;
+//middlewawres
+app.use(cors());
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+app.use("/movie", movieRouter);
+app.use("/booking", bookingsRouter);
+
+mongoose
+  .connect(
+    `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.6r6mwhf.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() =>
+    app.listen(5000, () =>
+      console.log("Connected to  Database and Server is running")
+    )
+  )
+  .catch((e) => console.log(e));
